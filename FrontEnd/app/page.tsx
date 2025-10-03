ee3e4892adc57d894d0b524571b4824bf724e7af
Rahilralu <nazarrahil0000@gmail.com>
2025-10-03 18:04:06 +0530

    Added initial files

---
+++ FrontEnd/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center  h-screen bg-amber-100">
+
+      
+      <h1 className="h1">
+        Store It-The only storage solution u need.
+      </h1>
+    </div>
+  );
+}
